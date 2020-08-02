ac8aa5ed68e150a10e903f2cea230768998010f6
nlemast <nlemaster47@gmail.com>
2020-08-02 12:17:27 -0400

    Create base server file with express

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/build', (req, res) => {
+	res.sendFile(path.join(__dirname, '../build/bundle.js'));
+});
+
+app.get('/', (req, res) => {
+	res
+		.status(200)
+		.type('html')
+		.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Listening on ${PORT}...`);
+});
