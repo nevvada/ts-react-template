1496fb70bd6cc7005961e9caf7e3e85d1cdabe4d
nlemast <nlemaster47@gmail.com>
2020-08-02 14:03:26 -0400

    Replace index.js file with index.tsx

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const environment = process.env.NODE_ENV || 'development';
 
 module.exports = {
     mode: environment,
-    entry: path.join(__dirname, 'src', 'index.js'),
+    entry: path.join(__dirname, 'src', 'index.tsx'),
     module: {
         rules: [
             {
