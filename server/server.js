640dab269f0225c569e343a20bb9f3c78a77b76b
nlemast <nlemaster47@gmail.com>
2021-03-16 17:41:05 -0400

    Update bundle, how html is rendered

--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const DIST_DIR = path.join(__dirname, '../dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+const PORT = 3000;
 
 const app = express();
 
-app.use('/dist', (req, res) => {
-	res.sendFile(path.join(__dirname, '../dist/bundle.js'));
-});
+app.use(express.static(DIST_DIR));
 
-app.get('/', (req, res) => {
-	res
-		.status(200)
-		.type('html')
-		.sendFile(path.join(__dirname, '../src/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(HTML_FILE);
 });
-
-const PORT = 3000;
 
 app.listen(PORT, () => {
-	console.log(`Listening on ${PORT}...`);
+  console.log(`Listening on ${PORT}...`);
 });
